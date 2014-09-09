4c559dd0e00449b7a832a8e898dcabcdee55b22d
Alexander Plavin <alexander@plav.in>
2014-09-09 20:20:27 +0400

    add links to words and topics; add Word page with HighCharts plots

--- web/main.py
+++ web/main.py
@@ -15,19 +15,59 @@ def overview():
 @app.route('/words')
 def words():
     with h5py.File('../data.hdf', mode='r') as h5f:
-        WordTuple = namedtuple('WordTuple', ['word', 'n', 'topics'])
+        WordTuple = namedtuple('WordTuple', ['w', 'word', 'n', 'topics'])
+        TopicTuple = namedtuple('TopicTuple', ['t', 'p'])
 
-        nw = h5f['n_wd'][...].sum(1).astype(int)
+        nw = h5f['n_wd'][...].sum(1)
         indices = nw.argsort()[:-20-1:-1]
         nws = nw[indices]
 
         words = h5f['dictionary'][...][indices]
 
         pts = h5f['p_wt'][...][indices,:]
-        topics = pts.argsort(axis=1)[:,:3]
+        topics = []
+        for w_pts in pts:
+            w_topics = w_pts.argsort()[:-4:-1]
+            topics.append(list( starmap(TopicTuple, zip(w_topics, w_pts[w_topics])) ))
 
-    return render_template('words.html', words=starmap(WordTuple, zip(words, nws, topics)))
+    return render_template('words.html', words=starmap(WordTuple, zip(indices, words, nws, topics)))
+
+
+@app.route('/word/<int:w>')
+def word(w):
+    WordTuple = namedtuple('WordTuple', ['w', 'word', 'n', 'topics', 'documents'])
+    TopicTuple = namedtuple('TopicTuple', ['t', 'p'])
+    DocumentTuple = namedtuple('DocumentTuple', ['d', 'n'])
+
+    ntop = 0
+    with h5py.File('../data.hdf', mode='r') as h5f:
+        nw = h5f['n_wd'][...].sum(1)[w]
+        word = h5f['dictionary'][...][w]
+
+        pts = h5f['p_wt'][...][w,:]
+        topics = pts.argsort()[::-1]
+        if ntop > 0:
+            topics = topics[:ntop]
+        topics = list( starmap(TopicTuple, zip(topics, pts[topics])) )
+
+        nds = h5f['n_wd'][...][w,:]
+        docs = nds.argsort()[::-1]
+        if ntop > 0:
+            docs = docs[:ntop]
+        docs = list( starmap(DocumentTuple, zip(docs, nds[docs])) )
+
+    return render_template('word.html', word=WordTuple(w, word, nw, topics, docs))
+
+
+@app.route('/topic/<int:t>')
+def topic(t):
+    pass
+
+
+@app.route('/document/<int:d>')
+def document(t):
+    pass
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
