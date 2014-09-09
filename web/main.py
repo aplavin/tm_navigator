5417f8b0faba4ad7010d71381aacaf70fdcc08fa
Alexander Plavin <alexander@plav.in>
2014-09-09 20:41:50 +0400

    refactor get_word_info and use it

--- web/main.py
+++ web/main.py
@@ -6,6 +6,10 @@ from itertools import starmap
 
 app = Flask(__name__)
 
+WordTuple = namedtuple('WordTuple', ['w', 'word', 'n', 'topics', 'documents'])
+TopicTuple = namedtuple('TopicTuple', ['t', 'p'])
+DocumentTuple = namedtuple('DocumentTuple', ['d', 'n'])
+
 
 @app.route('/')
 def overview():
@@ -15,48 +19,39 @@ def overview():
 @app.route('/words')
 def words():
     with h5py.File('../data.hdf', mode='r') as h5f:
-        WordTuple = namedtuple('WordTuple', ['w', 'word', 'n', 'topics'])
-        TopicTuple = namedtuple('TopicTuple', ['t', 'p'])
-
         nw = h5f['n_wd'][...].sum(1)
-        indices = nw.argsort()[:-20-1:-1]
-        nws = nw[indices]
+        indices = nw.argsort()[:-21:-1]
 
-        words = h5f['dictionary'][...][indices]
+        words = [get_word_info(w, h5f, 5) for w in indices]
 
-        pts = h5f['p_wt'][...][indices,:]
-        topics = []
-        for w_pts in pts:
-            w_topics = w_pts.argsort()[:-4:-1]
-            topics.append(list( starmap(TopicTuple, zip(w_topics, w_pts[w_topics])) ))
-
-    return render_template('words.html', words=starmap(WordTuple, zip(indices, words, nws, topics)))
+    return render_template('words.html', words=words)
 
 
 @app.route('/word/<int:w>')
 def word(w):
-    WordTuple = namedtuple('WordTuple', ['w', 'word', 'n', 'topics', 'documents'])
-    TopicTuple = namedtuple('TopicTuple', ['t', 'p'])
-    DocumentTuple = namedtuple('DocumentTuple', ['d', 'n'])
-
-    ntop = 0
     with h5py.File('../data.hdf', mode='r') as h5f:
-        nw = h5f['n_wd'][...].sum(1)[w]
-        word = h5f['dictionary'][...][w]
+        word = get_word_info(w, h5f, ntop=0)
 
-        pts = h5f['p_wt'][...][w,:]
-        topics = pts.argsort()[::-1]
-        if ntop > 0:
-            topics = topics[:ntop]
-        topics = list( starmap(TopicTuple, zip(topics, pts[topics])) )
+    return render_template('word.html', word=word)
 
-        nds = h5f['n_wd'][...][w,:]
-        docs = nds.argsort()[::-1]
-        if ntop > 0:
-            docs = docs[:ntop]
-        docs = list( starmap(DocumentTuple, zip(docs, nds[docs])) )
 
-    return render_template('word.html', word=WordTuple(w, word, nw, topics, docs))
+def get_word_info(w, h5f, ntop):
+    nw = h5f['n_wd'][...].sum(1)[w]
+    word = h5f['dictionary'][...][w]
+
+    pts = h5f['p_wt'][...][w,:]
+    topics = pts.argsort()[::-1]
+    if ntop > 0:
+        topics = topics[:ntop]
+    topics = list( starmap(TopicTuple, zip(topics, pts[topics])) )
+
+    nds = h5f['n_wd'][...][w,:]
+    docs = nds.argsort()[::-1]
+    if ntop > 0:
+        docs = docs[:ntop]
+    docs = list( starmap(DocumentTuple, zip(docs, nds[docs])) )
+
+    return WordTuple(w, word, nw, topics, docs)
 
 
 @app.route('/topic/<int:t>')
