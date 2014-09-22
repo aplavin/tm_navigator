a40eb73c73b1e0f950cdc700910082cbfa27fc46
Alexander Plavin <alexander@plav.in>
2014-09-22 16:50:15 +0400

    strip body and header from documents html

--- web/main.py
+++ web/main.py
@@ -243,8 +243,6 @@ def document(slug=None, d=None):
         filename = h5f['metadata']['filename', d]
         with codecs.open('static/docsdata/%s.html' % filename, encoding='utf-8') as f:
             html = f.read()
-        # html = re.search(r'<body>.*</body>', html, re.DOTALL).group(0)
-        # html = re.sub(r'<h(\d) id="[^"]+">', r'<h\1>', html)
 
         topics_used = Counter()
         html_new = ''
@@ -258,8 +256,13 @@ def document(slug=None, d=None):
             html_new += html[start:end]
             html_new += '</a></span>'
             html_pos = end
+        html_new += html[end:]
 
         html = html_new
+        html = re.search(r'<body>(.*)</body>', html, re.DOTALL).group(1)
+        header_m = re.search(r'</header>(.*)', html, re.DOTALL)
+        if header_m:
+            html = header_m.group(1)
 
         html = re.sub(r'<img class="(\w+)" src="\w+/(eqn\d+).png".*?/>',
                       r'<span class="sprite-\2"></span>',
