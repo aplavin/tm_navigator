20f431d45542c5236a539c197af60b2c80ea6e43
Alexander Plavin <alexander@plav.in>
2014-09-30 06:49:20 +0400

    cache datasets in data.py

--- web/data.py
+++ web/data.py
@@ -28,9 +28,23 @@ def zipnp(*arrs):
     return zip(*lsts)
 
 
-def get_nwd(h5f):
-    arr = h5f['n_wd_coo'][...]
-    return scipy.sparse.coo_matrix((arr['data'], (arr['row'], arr['col'])))
+datasets_cache = {}
+
+
+def get(name, sumaxis=None):
+    if (name, sumaxis) not in datasets_cache:
+        if name == 'nwd':
+            nwd = h5f['n_wd_coo'][...]
+            nwd = scipy.sparse.coo_matrix((nwd['data'], (nwd['row'], nwd['col'])))
+            data = nwd
+            if sumaxis is not None:
+                data = data.sum(axis=sumaxis).A1
+        else:
+            data = h5f[name][...]
+            if sumaxis is not None:
+                data = data.sum(axis=sumaxis)
+        datasets_cache[(name, sumaxis)] = data
+    return datasets_cache[(name, sumaxis)]
 
 
 WordTuple = recordtype('WordTuple', ['w', 'np', 'word', 'topics', 'documents'], default=None)
@@ -39,24 +53,24 @@ DocumentTuple = recordtype('DocumentTuple', ['d', 'np', 'meta', 'topics', 'words
 
 
 def get_words_all():
-    nws = get_nwd(h5f).sum(1).A1
+    nws = get('nwd', 1)
     ws = nws.argsort()[::-1]
-    words = h5f['dictionary'][...][ws]
+    words = get('dictionary')[ws]
     words = list( starmap(WordTuple, zip(ws, nws[ws], words)) )
     return words
 
 
 def get_documents_all():
-    nds = get_nwd(h5f).sum(0).A1
+    nds = get('nwd', 0)
     ds = nds.argsort()[::-1]
-    meta = h5f['metadata'][...][ds]
+    meta = get('metadata')[ds]
     docs = list( starmap(DocumentTuple, zip(ds, nds[ds], meta)) )
     return docs
 
 
 def get_topics_all():
-    nds = get_nwd(h5f).sum(0).A1
-    ptds = h5f['p_td'][...]
+    nds = get('nwd', 0)
+    ptds = get('p_td')
     pts = ptds.dot(1.0 * nds / nds.sum())
     ts = pts.argsort()[::-1]
     topics = list( starmap(TopicTuple, zip(ts, pts[ts])) )
@@ -64,15 +78,15 @@ def get_topics_all():
 
 
 def get_topics_info(ts, ntop=(-1, -1)):
-    pds = h5f['p_td'][...][ts,:]
-    nds = get_nwd(h5f).sum(0).A1
+    pds = get('p_td')[ts,:]
+    nds = get('nwd', 0)
     pt = pds.dot(1.0 * nds / nds.sum())
 
-    pws = h5f['p_wt'][...][:,ts].T
+    pws = get('p_wt')[:,ts].T
     ws = pws.argsort(axis=1)[:,::-1]
     if ntop[1] >= 0:
         ws = ws[:,:ntop[1]]
-    words = h5f['dictionary'][...][ws]
+    words = get('dictionary')[ws]
     words = [list( starmap(WordTuple, zipnp(ws_r, pws_r[ws_r], words_r)) )
              for ws_r, pws_r, words_r in zip(ws, pws, words)]
     words = map(
@@ -82,7 +96,7 @@ def get_topics_info(ts, ntop=(-1, -1)):
     docs = pds.argsort(axis=1)[:, ::-1]
     if ntop[0] >= 0:
         docs = docs[:, :ntop[0]]
-    docsmeta = h5f['metadata'][...][docs]
+    docsmeta = get('metadata')[docs]
     docs = [list( starmap(DocumentTuple, zipnp(docs_r, pds_r[docs_r], meta_r)) )
             for docs_r, pds_r, meta_r in zip(docs, pds, docsmeta)]
     docs = map(
@@ -99,10 +113,10 @@ def d_by_slug(slug):
 
 
 def get_docs_info(ds, ntop=(-1, -1)):
-    meta = h5f['metadata'][...][ds]
-    nd = get_nwd(h5f).sum(0).A1[ds]
+    meta = get('metadata')[ds]
+    nd = get('nwd', 0)[ds]
 
-    pts = h5f['p_td'][...][:,ds].T
+    pts = get('p_td')[:,ds].T
     topics = pts.argsort(axis=1)[:,::-1]
     if ntop[0] >= 0:
         topics = topics[:,:ntop[0]]
@@ -112,11 +126,11 @@ def get_docs_info(ds, ntop=(-1, -1)):
         lambda topics_r: filter(lambda t: t.np > 0, topics_r),
         topics)
 
-    nws = get_nwd(h5f).tocsc()[:,ds].A.T
+    nws = get('nwd').tocsc()[:,ds].A.T
     ws = nws.argsort()[:, ::-1]
     if ntop[1] >= 0:
         ws = ws[:,:ntop[1]]
-    words = h5f['dictionary'][...][ws]
+    words = get('dictionary')[ws]
     words = [list( starmap(WordTuple, zipnp(ws_r, nws_r[ws_r], words_r)) )
              for ws_r, nws_r, words_r in zip(ws, nws, words)]
     words = map(
@@ -177,16 +191,16 @@ def get_doc_content(doc):
 
 
 def w_by_word(word):
-    words = h5f['dictionary'][...]
+    words = get('dictionary')
     w = np.nonzero(words == word)[0][0]
     return w
 
 
 def get_words_info(ws, ntop=(-1, -1)):
-    nw = get_nwd(h5f).sum(1).A1[ws]
-    word = h5f['dictionary'][...][ws]
+    nw = get('nwd', 1)[ws]
+    word = get('dictionary')[ws]
 
-    pts = h5f['p_wt'][...][ws,:]
+    pts = get('p_wt')[ws,:]
     topics = pts.argsort(axis=1)[:,::-1]
     if ntop[0] >= 0:
         topics = topics[:,:ntop[0]]
@@ -196,11 +210,11 @@ def get_words_info(ws, ntop=(-1, -1)):
         lambda topics_r: filter(lambda t: t.np > 0, topics_r),
         topics)
 
-    nds = get_nwd(h5f).tocsr()[ws,:].A
+    nds = get('nwd').tocsr()[ws,:].A
     docs = nds.argsort(axis=1)[:,::-1]
     if ntop[1] >= 0:
         docs = docs[:,:ntop[1]]
-    docsmeta = h5f['metadata'][...][docs]
+    docsmeta = get('metadata')[docs]
     docs = [list( starmap(DocumentTuple, zipnp(docs_r, nds_r[docs_r], meta_r)) )
             for docs_r, nds_r, meta_r in zip(docs, nds, docsmeta)]
     docs = map(
