13f7bdc39f0af1f9819ec75b368d40a0f8acdcb4
Alexander Plavin <alexander@plav.in>
2014-10-19 09:09:05 +0400

    use piechart instead of long bar for similarity (now document only)

--- web/static/js/plots.js
+++ web/static/js/plots.js
@@ -117,6 +117,138 @@ function createLineChart(container, title, ttipNames, data) {
     });
 }
 
+Highcharts.SparkLine = function (options, callback) {
+    var defaultOptions = {
+        chart: {
+            renderTo: (options.chart && options.chart.renderTo) || this,
+            backgroundColor: null,
+            borderWidth: 0,
+            type: 'area',
+            margin: [2, 0, 2, 0],
+            width: 120,
+            height: 40,
+            style: {
+                overflow: 'visible'
+            },
+            skipClone: true
+        },
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            },
+            title: {
+                text: null
+            },
+            startOnTick: false,
+            endOnTick: false,
+            tickPositions: []
+        },
+        yAxis: {
+            endOnTick: false,
+            startOnTick: false,
+            labels: {
+                enabled: false
+            },
+            title: {
+                text: null
+            },
+            tickPositions: [0]
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            backgroundColor: 'white',
+            borderWidth: 1,
+            shadow: true,
+            useHTML: true,
+            hideDelay: 0,
+            shared: true,
+            padding: 0,
+            positioner: function (w, h, point) {
+                return { x: point.plotX - w / 2, y: point.plotY - h};
+            },
+            formatter: function labelf() {
+                if (!isNaN(this.x)) {
+                    return sprintf('<b>%s = %s</b>: %s = %.3f', options.titles[0], this.x, options.titles[1], this.y);
+                } else {
+                    return sprintf('<b>%s</b>: %s = %.3f', this.x, options.titles[1], this.y);
+                }
+            }
+        },
+        plotOptions: {
+            series: {
+                animation: false,
+                lineWidth: 1,
+                shadow: false,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                marker: {
+                    radius: 1,
+                    states: {
+                        hover: {
+                            radius: 2
+                        }
+                    }
+                },
+                fillOpacity: 0.25
+            },
+            column: {
+                negativeColor: '#eb6864',
+                borderColor: '#eb6864'
+            }
+        },
+        colors: ['#eb6864']
+    };
+    options = Highcharts.merge(defaultOptions, options);
+
+    return new Highcharts.Chart(options, callback);
+};
+
+$(function () {
+    $('.similarity-chart').each(function () {
+        var val = parseFloat($(this).data('value'));
+        $(this).highcharts('SparkLine', {
+            chart: {
+                type: 'pie',
+                height: 60
+            },
+            series: [{
+                data: [val, 1 - val]
+            }],
+            colors: ['#eb6864', '#eee'],
+            tooltip: {
+                enabled: false
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: false,
+                    dataLabels: {
+                        enabled: false
+                    }
+                },
+                series: {
+                    states: {
+                        hover: {
+                            enabled: false
+                        }
+                    }
+                }
+            }
+        });
+        $(this).attr('title', sprintf('Similarity: %d%%', val * 100));
+    });
+})
+
 Highcharts.setOptions({
     lang: {
         drillUpText: '<< Back'
