dfde4abd46088aa24b7c0a462f1c4b954de05d53
Alexander Plavin <alexander@plav.in>
2015-08-31 11:59:02 +0300

    move, add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='tm_navigator',
+    version='1.0',
+    packages=['tm_navigator'],
+    install_requires=[
+        'morepath',
+    ],
+    url='',
+    license='MIT',
+    author='Alexander Plavin',
+    author_email='alexander@plav.in',
+    description='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ]
+)
