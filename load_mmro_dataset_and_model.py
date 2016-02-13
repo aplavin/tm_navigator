c61d2f91a5b36316490bb82aa2835f5fcdf1d8dd
Kirill Bobyrev <kbobyrev@hotmail.com>
2016-02-13 15:31:46 +0300

    remove maintaining collision

--- load_mmro_dataset_and_model.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-os.system('./db_manage.py add_dataset')
-os.system('./db_manage.py load_dataset --dataset-id 1 --title "Simplest MMRO dataset" -dir data_mmro')
-os.system('./db_manage.py add_topicmodel --dataset-id 1')
-os.system('./db_manage.py load_topicmodel --topicmodel-id 1 --title "Simplest model" -dir data_mmro')
